a9c5427c20bbb68165b014eeea77806ff1580404
Ameer Hakim <hakimameer77@gmail.com>
2026-05-19 18:51:34 +0800

    Update leaderboard.js

--- arc os/leaderboard.js
+++ arc os/leaderboard.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 
-// Official verified configuration from your Firebase App Console
 const firebaseConfig = {
   apiKey: "AIzaSyAMThjhhw7jWb2pvYz5OFcnvMiTt6-Co",
   authDomain: "runner-system.firebaseapp.com",
@@ -13,27 +12,21 @@ const firebaseConfig = {
   measurementId: "G-2XH1NE0EPT"
 };
 
-// Initialize Firebase App & Database Services
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 const leaderboardTable = document.getElementById('leaderboard-data');
 
 const trackingRef = ref(database, 'tracking/');
 
-// Subscribing to Cloud Tree variations dynamically
 onValue(trackingRef, (snapshot) => {
     const data = snapshot.val();
     if (data) {
-        // Map raw object data properties into a linear array instance
         let runnersArray = Object.values(data);
         
-        // Sorting chronologically using epoch millisecond values (Fastest runner hits #1)
         runnersArray.sort((a, b) => a.raw_time - b.raw_time);
         
-        // Emptying table contents rows viewport data properties before injection loop
         leaderboardTable.innerHTML = "";
         
-        // Re-inject layout row structures cleanly 
         runnersArray.forEach((runner, index) => {
             let row = `<tr>
                 <td><strong>#${index + 1}</strong></td>
