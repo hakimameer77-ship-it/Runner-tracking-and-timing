671182190ff4ffd01a4f5adc9c6fba5a3ddc42cb
Ameer Hakim <hakimameer77@gmail.com>
2026-05-19 17:39:40 +0800

    Update leaderboard.js

--- arc os/leaderboard.js
+++ arc os/leaderboard.js
@@ -1,1 +1,37 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAMThjhhw7jWb2pvYz5OFcnvMiTt6-Co",
+  authDomain: "runner-system.firebaseapp.com",
+  databaseURL: "https://runner-system-default-rtdb.firebaseio.com",
+  projectId: "runner-system",
+  storageBucket: "runner-system.appspot.com",
+  messagingSenderId: "420895973613",
+  appId: "1:420895973613:web:5842b54c9fe82e061face4",
+  measurementId: "G-2XH1NE0EPT"
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const leaderboardTable = document.getElementById('leaderboard-data');
+
+const larianRef = ref(database, 'larian/');
+onValue(larianRef, (snapshot) => {
+    const data = snapshot.val();
+    if (data) {
+        let runnersArray = Object.values(data);
+        runnersArray.sort((a, b) => a.raw_time - b.raw_time);
+        leaderboardTable.innerHTML = "";
+        runnersArray.forEach((runner, index) => {
+            let row = `<tr>
+                <td><strong>#${index + 1}</strong></td>
+                <td>${runner.runner_id}</td>
+                <td>${runner.masa_imbasan}</td>
+            </tr>`;
+            leaderboardTable.innerHTML += row;
+        });
+    } else {
+        leaderboardTable.innerHTML = `<tr><td colspan="3" style="text-align: center; color: #999;">Tiada rekod data dijumpai.</td></tr>`;
+    }
+});
